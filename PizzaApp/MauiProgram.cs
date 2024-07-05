ebda65643a7ed369be119afb5e9cfc0711b63daf
Javier MC <javierm@maddevelopment.com>
2024-07-04 22:33:16 -0600

    Init app

---
+++ PizzaApp/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PizzaApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+                    Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+#elif STAGING
+                    Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Staging");
+#elif RELEASE
+                    Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Production");
+#endif
+
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
